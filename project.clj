0dc41d9f9a60c22ccac91edcc1bdc14add84c62f
Max Gonzih <gonzih@gmail.com>
2014-10-08 10:12:44 +0200

    Update feeds2imap version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-feeds2imap "0.1.2"
+(defproject lein-feeds2imap "0.1.3"
   :description "Leiningen plugin for feeds2imap.clj"
   :url "https://github.com/Gonzih/lein-feeds2imap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[feeds2imap "0.2.4"]])
+  :dependencies [[feeds2imap "0.2.5"]])
