d3b5e39dd2e37aa0e21356b1a3d0d5c49f913b6d
Max Gonzih <gonzih@gmail.com>
2014-10-08 09:24:40 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-feeds2imap "0.1.1"
+(defproject lein-feeds2imap "0.1.2"
   :description "Leiningen plugin for feeds2imap.clj"
   :url "https://github.com/Gonzih/lein-feeds2imap"
   :license {:name "Eclipse Public License"
