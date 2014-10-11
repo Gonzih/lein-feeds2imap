1ca311275c2b2e80762bcf76f09ac724296b488e
Max Gonzih <gonzih@gmail.com>
2014-10-11 14:16:27 +0200

    Fix logging issue by not using logging :)

--- src/leiningen/feeds2imap.clj
+++ src/leiningen/feeds2imap.clj
@@ -1,17 +1,5 @@
 (ns leiningen.feeds2imap
-  (:require [feeds2imap.core]
-            [clojure.tools.logging :refer [info]])
-  (:import [java.util Properties]
-           [org.apache.log4j PropertyConfigurator]))
-
-(defn configure-logger! []
-  (let [properties (Properties.)]
-    (doto properties
-        (.setProperty "log4j.rootLogger", "INFO, A1")
-        (.setProperty "log4j.appender.A1" "org.apache.log4j.ConsoleAppender")
-        (.setProperty "log4j.appender.A1.layout" "org.apache.log4j.PatternLayout")
-        (.setProperty "log4j.appender.A1.layout.ConversionPattern" "%m%n"))
-    (PropertyConfigurator/configure properties)))
+  (:require [feeds2imap.core]))
 
 (defn  ^:no-project-needed feeds2imap
   "Pull RSS/Atom feeds to your IMAP folders.
@@ -26,6 +14,4 @@
     opml2clj <file1> <file2> - convert OMPL file to feeds2imap format and store it in <file2>
     add <folder> <url> - add <url> to feeds, put in under <folder>"
   [_ & args]
-  (configure-logger!)
-  (info "Doing stuff")
   (apply feeds2imap.core/-main args))
