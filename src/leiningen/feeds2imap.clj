8cbf69701064eeb1b58d78753eb34e4b64d3fc60
Max Gonzih <gonzih@gmail.com>
2014-10-28 10:07:22 +0100

    Update documentation

--- src/leiningen/feeds2imap.clj
+++ src/leiningen/feeds2imap.clj
@@ -12,6 +12,12 @@
     show - show list of feeds
     opml2clj <file> - convert OMPL file to feeds2imap format
     opml2clj <file1> <file2> - convert OMPL file to feeds2imap format and store it in <file2>
-    add <folder> <url> - add <url> to feeds, put in under <folder>"
+    add <folder> <url> - add <url> to feeds, put in under <folder>
+
+    GPG based commands:
+    imap encrypt - encrypt your imap configuration using gpg
+    imap decrypt - decrypt your imap configuration using gpg
+
+    GPG commands require FEEDS_ENC_RECIPIENT env var to be set to the email address of the pub key that should be used."
   [_ & args]
   (apply feeds2imap.core/-main args))
