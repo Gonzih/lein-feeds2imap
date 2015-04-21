1218a9819fecc124c1c2d10465eb5376c07b624d
Max Gonzih <gonzih@gmail.com>
2015-04-21 14:08:05 +0200

    Bump version, use new release of feeds2imap

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-feeds2imap "0.2.0"
+(defproject lein-feeds2imap "0.2.1"
   :description "Leiningen plugin for feeds2imap.clj"
   :url "https://github.com/Gonzih/lein-feeds2imap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[feeds2imap "0.3.0"]])
+  :dependencies [[feeds2imap "0.3.1"]])
