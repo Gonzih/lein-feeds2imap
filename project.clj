321f57b6febfbff79acf94e004143ca85cade4ee
Max Gonzih <gonzih@gmail.com>
2017-12-01 10:52:00 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-feeds2imap "0.2.3"
+(defproject lein-feeds2imap "0.3.0"
   :description "Leiningen plugin for feeds2imap.clj"
   :url "https://github.com/Gonzih/lein-feeds2imap"
   :license {:name "The MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :eval-in-leiningen true
-  :dependencies [[feeds2imap "0.3.3"]])
+  :dependencies [[feeds2imap "0.4.0"]])
