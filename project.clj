dae9b53d4488047630dde406ff99a68ef2febc0e
Max Gonzih <gonzih@gmail.com>
2017-12-08 23:34:06 +0100

    Release the new version with the latest deps

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-feeds2imap "0.4.1-1"
+(defproject lein-feeds2imap "0.4.2-1"
   :description "Leiningen plugin for feeds2imap.clj"
   :url "https://github.com/Gonzih/lein-feeds2imap"
   :license {:name "The MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :eval-in-leiningen true
-  :dependencies [[feeds2imap "0.4.1"]])
+  :dependencies [[feeds2imap "0.4.2"]])
